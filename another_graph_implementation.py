2232c62a865c47c7742f2ca9f3f324b3921333d1
Annie Kramer <anniekramer@dropbox.com>
2016-11-08 15:29:31 -0800

    Add graph files

---
+++ another_graph_implementation.py
@@ -0,0 +1,80 @@
+'''
+Example graph data:
+
+6 nodes here: A, B, C, D, E, F
+Each node represented by a key in a dictionary.
+For each key in the dict, the value is a list that contains the nodes connected to the start node (key).
+
+
+    graph = {'A': ['B', 'C'],
+             'B': ['C', 'D'],
+             'C': ['D'],
+             'D': ['C'],
+             'E': ['F'],
+             'F': ['C']}
+'''
+
+def find_first_path_between_nodes(graph, start, end, path=[]):
+    # Add start node to path array
+    path = path + [start]
+
+    # If the start and end nodes are the same, just return a list that simply
+    # contains that starting node.
+    if start == end:
+        return path
+
+    # Check whether or not the graph object contains the given start key.
+    if not graph.has_key(start):
+        return None
+
+    # For a given node in the list of nodes corresponding to the start node key,
+    # check if the node is in the path list. If note, create a new path and return it.
+    for node in graph[start]:
+        if node not in path:
+            new_path = find_first_path_between_nodes(graph, node, end, path)
+            
+            if new_path:
+                return new_path
+
+    return None
+
+def find_all_paths_between_nodes(graph, start, end, path=[]):
+    path = path + [start]
+
+    if start == end:
+        return path
+
+    if not graph.has_key(start):
+        return []
+
+    paths = []
+
+    for node in graph[start]:
+        if node not in path:
+            new_paths = find_all_paths_between_nodes(graph, node, end, path)
+
+            for new_path in new_paths:
+                paths.append(new_path)
+
+    return paths
+
+def find_shortest_path_between_nodes(graph, start, end, path=[]):
+    path = path + [start]
+
+    if start == end:
+        return path
+
+    if not graph.has_key(start):
+        return None
+
+    shortest_path_between_nodes = None
+
+    for node in graph[start]:
+        if node not in path:
+            new_path = find_shortest_path_between_nodes(graph, node, end, path)
+
+            if new_path:
+                if not shortest or len(new_path) < len(shortest):
+                    shortest_path_between_nodes = new_path
+
+    return shortest_path_between_nodes
---
+++ undirected_graph.py
@@ -0,0 +1,47 @@
+from collections import defaultdict
+
+class Graph(object):
+
+    def _init_(self, connections, directed=False):
+        self._graph = defaultdict(set)
+        self._directed = directed
+        self.add_connections(connections)
+
+    def add_connections_to_graph(self, connections):
+        for node1, node2 in connections:
+            self.add(node1, node2)
+
+    def add_connection_between_nodes:
+        self._graph[node1].add(node2)
+        if not self._directed:
+            self._graph[node2].add(node1)
+
+    def remove_references_to_node:
+        for node, connections in self._graph.iteritems():
+            try:
+                connections.remove(node)
+            except KeyError:
+                pass
+        try:
+            del self._graph[node]
+        except KeyError:
+                pass
+
+    def is_connected_nodes(self, node1, node2):
+        return node1 in self._graph and node2 in self._graph[node1]
+
+    def find_shortest_path_between_nodes(self, node1, node2, path=[]):
+        path = path + node1
+        if node1 == node2:
+            return path
+        if node1 not in self._graph:
+            return None
+        for node in graph[node1]:
+            if node not in path:
+                new_path = self.find_path(node1, node2)
+                if new_path:
+                    return new_path
+        return None
+
+    def __str__(self):
+        return '{}({})'.format(self.__class__.__name__, dict(self._graph))
